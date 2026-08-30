c8e6b6d1e8c3631dcfdc82d8b51200c64a42ccba
kielsvu <jhoneragustin@gmail.com>
2026-08-31 00:34:15 +0800

    Remove WebkitUserDrag style from HeroVisual

--- src/components/HeroVisual.tsx
+++ src/components/HeroVisual.tsx
@@ -19,7 +19,6 @@ export default function HeroVisual() {
             userSelect: "none",
             WebkitUserSelect: "none",
             WebkitTouchCallout: "none",
-            WebkitUserDrag: "none",
             touchAction: "none",
           }}
         >
