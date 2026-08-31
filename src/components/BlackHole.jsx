b6cd3bcba47be924bb97aa05886a43e8c7a7c920
kielsvu <jhoneragustin@gmail.com>
2026-08-31 14:50:17 +0800

    Increase particle count from 1000 to 9000

--- src/components/BlackHole.jsx
+++ src/components/BlackHole.jsx
@@ -5,7 +5,7 @@ import { useRef, useEffect, useCallback, useState } from "react"
 const PERSPECTIVE = 1300
 
 const DEFAULTS = {
-  particleCount: 1000,
+  particleCount: 9000,
   particleSize: 3,
   colors: ["#B284FF", "#D6BEFF", "#FFFFFF"],
   outerRadius: 100,
